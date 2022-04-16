d4a314bd974aa4e3bbce89cd5eea852376a22d92
Ricardo Serrato B <rsbse@hotmail.com>
2022-04-16 16:28:12 -0500

    Lee Perry Smith

--- LeePerrySmith/index.js
+++ LeePerrySmith/index.js
@@ -4,8 +4,9 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-let threePath = path.join(__dirname, 'node_modules/three');
-app.use('/three/', express.static(threePath))
+let threePath = path.join(__dirname, 'node_modules');
+console.log(threePath);
+app.use('/node_modules/', express.static(threePath))
 
 app.listen(3000, () => {
     console.log("Success")
--- LeePerrySmith/public/main.js
+++ LeePerrySmith/public/main.js
@@ -1,8 +1,8 @@
-import * as THREE from './three/build/three.module.js';
-import GLTFLoader from './three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from './node_modules/three/build/three.module.js';
+import * as TEMP from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 
-const loader = new GLTFLoader();
+//const loader = new GLTFLoader();
 
 /*loader.load('https://threejs.org/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb', function (gltf) {
 
