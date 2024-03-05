cc75f7a2bb65dcbbd0d7c65da0f7109f44557566
Ricardo Serrato B. <rsbse@hotmail.com>
2024-03-05 07:44:42 -0600

    Changes

--- Rotating2DObject/main.js
+++ Rotating2DObject/main.js
@@ -8,26 +8,26 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const figure = createFigure();
+const figure = createFigure(5);
 var angleCamera = 0;
 var radiusCamera = 10;
 scene.add( figure );
 
-camera.position.z = 10;
+camera.position.z = 5;
 camera.position.y = 0;
-camera.position.x = 10;
+camera.position.x = 0;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-    camera.position.z = radiusCamera * Math.cos(angleCamera);
-    camera.position.x = radiusCamera * Math.sin(angleCamera);
-    angleCamera = angleCamera + 0.9;
+    camera.position.x = radiusCamera * Math.cos(angleCamera);
+    camera.position.z = radiusCamera * Math.sin(angleCamera);
+    angleCamera = angleCamera + 0.8;
     camera.lookAt(new THREE.Vector3(0,0,0))
 	renderer.render( scene, camera );
 }
 
-function createFigure() {
+function createFigure(k) {
     const material = new THREE.LineBasicMaterial({
         color: 0xffffff
     });
@@ -35,8 +35,8 @@ function createFigure() {
     const points = [];
     
     for (let angle = 0; angle < Math.PI * 2; angle+=0.001) {
-        let x = 2* Math.cos(angle);
-        let y = 2* Math.sin(angle);
+        let x = 5* 1 * Math.cos(k*angle) * Math.cos(angle);
+        let y = 5* 1 * Math.cos(k*angle) * Math.sin(angle);
        
         points.push(new THREE.Vector3(x, y, 0));
     }
@@ -44,7 +44,7 @@ function createFigure() {
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
     
     const line = new THREE.Line( geometry, material );
-    scene.add( line );
+    return line;
 }
 
 animate();
